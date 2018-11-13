a4982aba72c7f0a261ae812bda2be6706fab3550
venkatesh444 <veeravenkatesh.loya@gmail.com>
2018-11-13 16:24:21 +0530

    the first select code

---
+++ src/test/java/com/qa/test/DemoTest2.java
@@ -0,0 +1,34 @@
+package com.qa.test;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class DemoTest2 {
+
+	@Test
+	public void sum()
+	{
+		System.out.println("ADD");
+		int a=3;
+		int b=5;
+		Assert.assertEquals(8, a+b);
+	}
+	
+	@Test
+	public void sub()
+	{
+		System.out.println("SUB");
+		int a=5;
+		int b=3;
+		Assert.assertEquals(2, a-b);
+	}
+	
+	@Test
+	public void mul()
+	{
+		System.out.println("MUL");
+		int a=3;
+		int b=5;
+		Assert.assertEquals(15, a*b);
+	}
+}
